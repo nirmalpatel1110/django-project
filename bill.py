84ed060b3843aa9e33be42f02b0f5f53eca33d0b
kevin942003 <108712895+kevin942003@users.noreply.github.com>
2022-09-06 08:47:43 +0530

    Add files via upload

--- bill.py
+++ bill.py
@@ -1,8 +1,38 @@
-from django.urls import path
-from ..views.bill import *
-billURL=[
-    path('viewBill/',viewBill,name="viewBill"),
-    path('addBill/',addBill,name="addBill"),  
-    path('updateBill/<id>',updateBill,name="updateBill"), 
-    path('deleteBill/<id>',deleteBill,name="deleteBill"), 
-]
+from django.shortcuts import get_object_or_404,render,redirect
+from ..models import billModel
+from django.shortcuts import render
+#from ..models import Category as CategoryForm
+from ..forms.Bill_form import BillForm
+#from ..models import Category
+
+def viewBill(request):
+    context={}
+    context["Bills"]=billModel.objects.all()
+    return render(request, "Bill/view.html",context)
+
+def addBill(request):
+    context={}
+    form=BillForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect( "viewBill")
+    context['form']=form
+    return render(request,"Bill/add.html",context)
+
+def updateBill(request,id):
+    context={}
+    obj=get_object_or_404(billModel,id=id)
+    form=BillForm(request.POST or None,instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect("viewBill")
+    context["form"]=form
+    return render(request,"Bill/edit.html",context)
+
+def deleteBill(request,id):
+    context={}
+    obj=get_object_or_404(billModel,id=id)
+    if request.method=="GET":
+        obj.delete()
+        return redirect("viewCategory")
+    return render(request,"category/view.html",context)
